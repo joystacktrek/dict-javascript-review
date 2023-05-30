f40ed7af109db95cccc6c308ad117f72fdccf937
joy2dworld <115624740+joy2dworld@users.noreply.github.com>
2023-05-30 21:32:20 +0800

    initial commit

---
+++ javascript-basics.js
@@ -0,0 +1,34 @@
+// best practices
+
+// keywords: var, let, const
+// initializations
+const birthYear = 1991 
+let currentYear = 2023
+
+// data types
+// string, integer, float, boolean, arrays, objects
+let name = '4141'
+let greeting = 'Hi'
+let age = 272298928938721983
+let allowance = 1000.50
+let isGwapa = true // case sensitive
+let isLaway = false
+
+// array
+// 0,1,2,3....-> index
+let scholars = ['emman', 'emeroe', 'eugene']
+console.log(scholars[2])
+
+// objects: key-value pair
+let students = {name: 'emman', age: 24}
+console.log(students['name'])
+
+
+
+// displays something
+console.log(birthYear)
+console.log(currentYear) // 2023
+
+
+
+// technical debt
